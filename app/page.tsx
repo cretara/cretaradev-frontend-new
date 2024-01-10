1862cff4576e86a599d634d99ee529cc7d8b6a0e
cretara <pierdanielecretara@lviiier.com>
2024-01-10 07:46:22 +0100

    Added Titillium global font

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Body from "@/components/Body";
+import { Titillium_Web } from "next/font/google";
 
-export default async function IndexCretara() {
+const titillium = Titillium_Web({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export default function CretaraDev() {
   return (
-    <div id={"main"}>
+    <div id={"mainCretaraDev"} className={titillium.className}>
       <Body />
     </div>
   );
