133018a48e4c205cff60688bd2a31c012c4d0952
cretara <pierdanielecretara@lviiier.com>
2024-04-15 22:57:37 +0200

    Updated all remaining packages with ncu and fixed about/page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,13 @@
-export default async function Page() {
+export default function Page() {
   return (
-    <main>
-      <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-        Cretara Pier Daniele
-      </h1>
+    <section
+      id="about"
+      className="relative z-10 overflow-hidden pt-[120px] pb-16 md:pt-[150px] md:pb-[120px] xl:pt-[180px] xl:pb-[160px] 2xl:pt-[210px] 2xl:pb-[200px]"
+    >
+      <h1 className="">Cretara Pier Daniele</h1>
       <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
         I'm a software engineer with more than 17 years of experience...
       </p>
-    </main>
+    </section>
   );
 }
