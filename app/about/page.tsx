27eff906b8a65d8824b86a5ec52b9c780f99336e
cretara <cretaradev@gmail.com>
2025-06-27 21:50:56 +0200

    Migration to Tailwind version 4 done

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
       className="relative z-10 overflow-hidden pt-[120px] pb-16 md:pt-[150px] md:pb-[120px] xl:pt-[180px] xl:pb-[160px] 2xl:pt-[210px] 2xl:pb-[200px]"
     >
       <h1 className="">Cretara Pier Daniele</h1>
-      <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
+      <p className="mb-12 text-base font-medium leading-relaxed! text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
         I'm a software engineer with more than 17 years of experience...
       </p>
     </section>
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -40,7 +40,7 @@ const Header = () => {
       <header
         className={`header top-0 left-0 z-40 flex-1 w-full items-center bg-transparent ${
           sticky
-            ? "!fixed !z-[9999] !bg-white !bg-opacity-80 shadow-sticky backdrop-blur-sm !transition dark:!bg-primary dark:!bg-opacity-20"
+            ? "fixed! z-9999! bg-white! !bg-opacity-80 shadow-sticky backdrop-blur-xs transition! dark:!bg-primary dark:!bg-opacity-20"
             : "absolute"
         }`}
       >
@@ -95,7 +95,7 @@ const Header = () => {
                 </button>
                 <nav
                   id="navbarCollapse"
-                  className={`navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white py-4 px-6 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:!bg-transparent lg:p-0 lg:opacity-100 ${
+                  className={`navbar absolute right-0 z-30 w-[250px] rounded border-[.5px] border-body-color/50 bg-white py-4 px-6 duration-300 dark:border-body-color/20 dark:bg-dark lg:visible lg:static lg:w-auto lg:border-none lg:bg-transparent! lg:p-0 lg:opacity-100 ${
                     navbarOpen
                       ? "visibility top-full opacity-100"
                       : "invisible top-[120%] opacity-0"
@@ -137,7 +137,7 @@ const Header = () => {
                                 <Link
                                   href={submenuItem.path ?? ""}
                                   key={submenuItem.id}
-                                  className="block rounded py-2.5 text-sm text-dark hover:opacity-70 dark:text-white lg:px-3"
+                                  className="block rounded-sm py-2.5 text-sm text-dark hover:opacity-70 dark:text-white lg:px-3"
                                 >
                                   {submenuItem.title}
                                 </Link>
