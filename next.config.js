0f708b68e3943b525265770caca8c941806efb0b
cretara <cretaradev@gmail.com>
2025-12-30 00:10:25 +0100

    chore: update Next to the latest stable 16.1.1, Typescript, and ESLint; adjust image handling in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**',
-            },
-            {
-                protocol: 'http',
-                hostname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'cretara.dev',
-            },
-        ],
-        // Alternative for stricter production config:
-        domains: ['cretara.dev'],
-    },
-    reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "cretara.dev",
+      },
+    ],
+  },
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
